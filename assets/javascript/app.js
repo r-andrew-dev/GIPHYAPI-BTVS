adb751b095d90127b5f050ab282b5020b61ae0c0
Rachael Andrew <r.andrew.dev@gmail.com>
2020-01-15 23:50:09 -0600

    added changing user message depending on search entered, ensuring search field empties either way after clicking submit

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,126 +1,111 @@
-$(function() {
+$(function () {
 
-var button;
+    var button;
 
-var topics = ["buffy", "spike", "willow", "xander", "giles", "anyanka", "tara", "joyce", "angelus", "oz", "faith"];
+    var topics = ["buffy", "spike", "willow", "xander", "giles", "anyanka", "tara", "joyce", "angelus", "oz", "faith"];
 
-function create() {
-    
-    for (i=0; i < topics.length; i++) {
+    function create() {
 
-    button = $("<button>")
+        for (i = 0; i < topics.length; i++) {
 
-    button.text(topics[i])
+            button = $("<button>")
 
-    $("#buttons").append(button);
+            button.text(topics[i])
 
-}
-
-}
-
-create();
-
-$("#buttons").on("click", "button", function() {
-    
-    var query = $(this).text().toLowerCase();
-
-    var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=cqvd7dfuQogEffYZG7FyYxZHBWtlG86P&q="
-     + query + " buffy&limit=10&offset=0&rating=PG-13&lang=en";
-
-    $.ajax({
-        url: queryURL,
-        method: "GET"
-
-
-    }).then(function(response) {
-
-        $("#image-container").html(" ");
-
-        var results = response.data;
-
-        for (i=0; i < results.length; i++) {
-
-        var gifDiv = $("<div>");
-
-        var p = $("<p>");
-
-        var rating = results[i].rating
-
-        var newRating = rating.toUpperCase()
-
-        p.text("Rating: " + (newRating));
-
-        var image = $("<img>");
-
-        image.attr("src", results[i].images.fixed_height_still.url);
-
-        image.attr("data-state", "still")
-
-        image.attr("data-animate", results[i].images.fixed_height.url);
-
-        image.attr("data-still", results[i].images.fixed_height_still.url);
-
-        image.addClass("gif");
-
-        gifDiv.append(image);
-        gifDiv.append(p);
-
-        $("#image-container").prepend(gifDiv);
-
-
+            $("#buttons").append(button);
         }
+    }
+
+    create();
+
+    $("#buttons").on("click", "button", function () {
+
+        var query = $(this).text().toLowerCase();
+
+        var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=cqvd7dfuQogEffYZG7FyYxZHBWtlG86P&q="
+            + query + " buffy&limit=10&offset=0&rating=PG-13&lang=en";
+
+        $.ajax({
+            url: queryURL,
+            method: "GET"
 
 
+        }).then(function (response) {
+
+            $("#image-container").html(" ");
+
+            var results = response.data;
+
+            for (i = 0; i < results.length; i++) {
+
+                var gifDiv = $("<div>");
+
+                var p = $("<p>");
+
+                var rating = results[i].rating
+
+                var newRating = rating.toUpperCase()
+
+                p.text("Rating: " + (newRating));
+
+                var image = $("<img>");
+
+                image.attr("src", results[i].images.fixed_height_still.url);
+
+                image.attr("data-state", "still")
+
+                image.attr("data-animate", results[i].images.fixed_height.url);
+
+                image.attr("data-still", results[i].images.fixed_height_still.url);
+
+                image.addClass("gif");
+
+                gifDiv.append(image);
+                gifDiv.append(p);
+
+                $("#image-container").prepend(gifDiv);
+            }
+        })
     })
 
+    $("#image-container").on("click", ".gif", function () {
 
-})
+        console.log(this);
 
-$("#image-container").on("click", ".gif", function() {
+        if ($(this).attr("data-state") === "still") {
 
-    console.log(this);
+            $(this).attr("src", $(this).attr("data-animate"));
+            $(this).attr("data-state", "animate");
 
-    if ($(this).attr("data-state") === "still") {
+            console.log($(this).attr("data-animate"));
+        }
+        else {
 
-        $(this).attr("src", $(this).attr("data-animate"));
-        $(this).attr("data-state", "animate");
+            $(this).attr("src", $(this).attr("data-still"));
+            $(this).attr("data-state", "still");
+        }
+    })
 
-        console.log($(this).attr("data-animate"));
+    $(".submit").on("click", function () {
+        event.preventDefault();
+        var buttonText = $(".search").val().trim().toLowerCase();
 
-    }
-    else {
+        if (topics.indexOf(buttonText) > -1) {
 
-        $(this).attr("src", $(this).attr("data-still"));
-        $(this).attr("data-state", "still");
-    }
+            $("#user-flag").text("Don't you see it up there already?");
+            $(".search").val("");
+            console.log(topics.indexOf(buttonText))
+            return "This button has already been added."
+        }
 
+        else {
 
-})
-
-$(".submit").on("click", function() {
-    event.preventDefault();
-    var buttonText = $(".search").val().trim().toLowerCase();
-
-    if (topics.indexOf(buttonText) > -1) {
-
-        console.log(topics.indexOf(buttonText))
-        return "This button has already been added."
-    }
-
-    else {
-
-    topics.push(buttonText);
-    $(".search").val("");
-    $("#buttons").empty();
-    create();
-    console.log(topics.indexOf("buffy"));
-
-    }
-
-
-});
-
-
-
-
+            $("#user-flag").text("You're so awesome")
+            topics.push(buttonText);
+            $(".search").val("");
+            $("#buttons").empty();
+            create();
+            console.log(topics.indexOf("buffy"));
+        }
+    });
 });
